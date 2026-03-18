4f73a8397210c81a34f579c8d2770eb96685b137
StefanRuseski <ruseskis@gmail.com>
2026-03-18 18:32:16 +0100

    Homework1 - Task3

---
+++ Homework1 - Task3/SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Input the first number
+        Console.Write("Input the First Number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        // Input the second number
+        Console.Write("Input the Second Number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+        // Swap the numbers
+        int temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+        // Output the results after swapping
+        Console.WriteLine("\nAfter Swapping:");
+        Console.WriteLine($"First Number: {firstNumber}");
+        Console.WriteLine($"Second Number: {secondNumber}");
+    }
+}
+
